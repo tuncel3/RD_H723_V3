9b17fe928898ab611a05278c8fac7c19db6c9ca1
tuncel3 <tuncel3@gmail.com>
2025-04-23 01:43:34 +0300

    Auto commit on 2025-04-23 01:43 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -751,20 +751,20 @@ if (sfsta_op_phase == S_SFSTA_REQ_OK) {
 		blm_cancel_op_return_normal();
 		sprintf(DUB,"blm Vbat too low. batt broken set"); umsg(blm_u, DUB);
 	}
-	if ((IBAT_pas.a16) > blm_I_step_075perc) {
+	if (fabs(IBAT_pas.a16) > blm_I_step_075perc) {
 		batt_current_detected_cnt++;
 		if (batt_current_detected_cnt >= batt_current_detected_per) {
 			batt_current_detected_cnt=0;
 			batt_current_detected=1;														// CURRENT DETECTED
-		} else { batt_current_detected_cnt=0; }
-	}
-	if ((IBAT_pas.a16) <= blm_I_step_075perc) {
+		}
+	} else { batt_current_detected_cnt=0; }
+	if (fabs(IBAT_pas.a16) <= blm_I_step_075perc) {
 		batt_current_detected_cnt--;
 		if (batt_current_detected_cnt <= -batt_current_detected_per) {
 			batt_current_detected_cnt=0;
 			batt_current_detected=0;														// CURRENT DETECTED CANCELED
-		} else { batt_current_detected_cnt=0; }
-	}
+		}
+	} else { batt_current_detected_cnt=0; }
 	if (!irect_stable) {		// rectifier akımındaki oynama bat akımında oynamaya neden olup operasyonu bozabiliyor.
 		blm_discard_corr_restart_normal();
 	}
