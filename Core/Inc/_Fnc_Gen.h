fc6c8b9185c9e32dd73ee8448cf0416d244bc0f7
tuncel3 <tuncel3@gmail.com>
2025-04-22 12:19:26 +0300

    Auto commit on 2025-04-22 12:19 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -1198,7 +1198,7 @@ void stability_ibat_fc(void) {
         ibat_stable_cnt++;
     }
     ibat_stable = 		    (ibat_stable_cnt >= 150);
-    batt_current_detected = (ibat_stable) && fabs(IBAT_pas.a16 > blm_I_step_05percx2); // bat akımı stabil ve yok thresholdu dışında. yani var.
+    batt_current_detected = (ibat_stable) && fabs(IBAT_pas.a16) > blm_I_step_05percx2; // bat akımı stabil ve yok thresholdu dışında. yani var.
 }
 
 void blm_set_up_down_vtarg_limits(void) {
