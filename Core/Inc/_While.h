e080b99688fec182090ce379aa66535ee2217b0e
tuncel3 <tuncel3@gmail.com>
2025-04-23 08:16:09 +0300

    Commit on 2025-04-23 08:15 - blm yeter. iyi durumda. diğer şeylere geçilebilir.

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -861,7 +861,7 @@ if (sfsta_op_phase == S_SFSTA_REQ_OK) {
 			blm_corr_results[blm_corr_results_index]=blm_corr;	// corr ları kaydet. bir yerde kullanılmıyor.
 				sprintf(DUB,"  blm_corr %f", blm_corr); umsg(blm_u, DUB);
 			if (discard_corr_result == 0) {
-				if (blm_corr >= 0.90) {	// bir tanesi 0.9 üsü ise corr ok.
+				if (blm_corr >= 0.90) {	// bir tanesi 0.9 üstü ise corr ok.
 					blm_req_corr_batt_connected=1;
 					sprintf(DUB,"corr good. 0.90 batt connected."); umsg(blm_u, DUB);
 				} else if (blm_corr >= 0.85 && blm_corr_p >= 0.85) { // son ikisi 0.85 üstü ise corr ok
