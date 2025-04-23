f86dd0ceaefc72b1accc7b4b366178007af0ee0a
tuncel3 <tuncel3@gmail.com>
2025-04-23 04:01:36 +0300

    Auto commit on 2025-04-23 04:01 - auto

--- Core/Inc/_DefConst.h
+++ Core/Inc/_DefConst.h
@@ -1270,7 +1270,6 @@ float blm_vi_change_mult = 0.001f;
 float blm_corr = 0.0f;
 uint32_t blm_batt_connected_0_cnt = 0;
 uint8_t discard_corr_result = 0;
-uint8_t blm_returning_to_charge_voltşage = 0;
 uint8_t blm_restart_after_return = 0;
 
 int sfsta_op_phase = 0;
