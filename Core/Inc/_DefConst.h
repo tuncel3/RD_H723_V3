79747c87c99b79c2037041e3579fb3cae3e5946d
tuncel3 <tuncel3@gmail.com>
2025-04-25 00:29:46 +0300

    Auto commit on 2025-04-25 00:29 - auto

--- Core/Inc/_DefConst.h
+++ Core/Inc/_DefConst.h
@@ -1,21 +1,19 @@
 
 
-//#define DEBUG_GENERAL		1	// seçili olan gruplar uart da yazılır
-//#define DEBUG_BLM   		1
-//#define UART_DEBUG_FAULT	0
-//#define UART_DEBUG_TEMP		0
-//
-//#if DEBUG_GENERAL
-//  #define PRF_GEN(...)  { sprintf(DUB, __VA_ARGS__); prfm(DUB); }
-//#else
-//  #define PRF_GEN(...)
-//#endif
-//
-//#if DEBUG_BLM
-//  #define PRF_GEN(...)    { sprintf(DUB, __VA_ARGS__); prfm(DUB); }
-//#else
-//  #define PRF_GEN(...)
-//#endif
+#define DEBUG_GENERAL		1	// seçili olan gruplar uart da yazılır
+#define DEBUG_BLM   		1
+
+#if DEBUG_GENERAL
+  #define PRF_GEN(...)  { sprintf(DUB, __VA_ARGS__); prfm(DUB); }
+#else
+  #define PRF_GEN(...)
+#endif
+
+#if DEBUG_BLM
+  #define PRF_GEN(...)    { sprintf(DUB, __VA_ARGS__); prfm(DUB); }
+#else
+  #define PRF_GEN(...)
+#endif
 
 volatile uint8_t dbg_gen = 1;
 volatile uint8_t dbg_blm = 0;
