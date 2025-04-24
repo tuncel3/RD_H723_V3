178aa7e575306190feef2e1109fb6294425ee957
tuncel3 <tuncel3@gmail.com>
2025-04-24 23:17:45 +0300

    Auto commit on 2025-04-24 23:17 - auto

--- Core/Inc/_DefConst_OK.h
+++ Core/Inc/_DefConst_OK.h
@@ -147,7 +147,6 @@ uint8_t buffer_tail = 0;  // Read pointer
 uint8_t dma_busy = 0;     // DMA busy flag
 
 char DUB[128]={0};
-char UXPUB[54]={0};
 
 #define ubl1_    (1 << 0)  // Bit 0	general message
 #define blm_u    (1 << 1)  // Bit 1 uart message batt line
