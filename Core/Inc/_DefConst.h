aba65300b6fd2f069f8a02a95776ff639705a7b0
tuncel3 <tuncel3@gmail.com>
2025-04-24 15:29:19 +0300

    Auto commit on 2025-04-24 15:29 - auto

--- Core/Inc/_DefConst.h
+++ Core/Inc/_DefConst.h
@@ -596,6 +596,11 @@ uint32_t cal_sel_edit_mode=0;
 
 uint8_t rectf_active_at_startup_req_right = 0;
 
+typedef enum {
+	dropper_limits,
+	S_STARTUP_DELAY_OK
+}set_variables_from_EEP;
+set_variables_from_EEP sta_op_phase = 0;
 
 
 float array_settings_data[NUM_SET_ENUM][2]={{0.0f, 0.0f}};
--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -765,7 +765,7 @@ void inline extern aku_hatti_kopuk_fc_inl(void) {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void inline extern set_variables_from_EEP_fc(void) { // n012
+void inline extern set_variables_from_EEP_fc(uint8_t scope) { // n012
 Vdc_float_min=EpD[DEV_NOM_VOUT][0].V1*0.9; // Normal şarj rejimi gerilim ayar aralığı
 Vdc_float_max=EpD[DEV_NOM_VOUT][0].V1*1.15; // Normal şarj rejimi gerilim ayar aralığı
 Vdc_boost_min=EpD[DEV_NOM_VOUT][0].V1*1.15; // Tam şarj rejimi gerilim ayar aralığı
@@ -773,8 +773,12 @@ Vdc_boost_max=EpD[DEV_NOM_VOUT][0].V1*1.3; // Tam şarj rejimi gerilim ayar aral
 
 // D.A. gerilim regülasyonu (dropping diyot)
 // limitler şartnamede anma geriliminin yüzdesi olarak belirtilmiş.
+//if (scope==dropper_limits) {
 //Vdc_drop_in_min=EpD[DEV_NOM_VOUT][0].V1*0.9; // D.A. gerilim regülasyonu giriş gerilimi
 //Vdc_drop_in_max=EpD[DEV_NOM_VOUT][0].V1*1.3; // D.A. gerilim regülasyonu giriş gerilimi
+dropp_reg_high_lim=EpD[DEV_NOM_VOUT][0].V1*(1+(EpD[dropp_reg_high_lim_add][0].V1/100)); // D.A. gerilim regülasyonu çıkış gerilimi
+dropp_reg_low_lim=EpD[DEV_NOM_VOUT][0].V1*(1-(EpD[dropp_reg_low_lim_sub][0].V1/100)); // D.A. gerilim regülasyonu çıkış gerilimi
+//}
 
 // D.A. çıkış akım ayarı
 Irect_max=EpD[DEV_NOM_IOUT][0].V1*1.0; // Toplam çıkış
