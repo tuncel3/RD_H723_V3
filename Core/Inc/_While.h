4bb65d8175a5c7d7f4fdbd226cce5deedfdf277b
tuncel3 <tuncel3@gmail.com>
2025-04-24 07:14:16 +0300

    Auto commit on 2025-04-24 07:14 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -259,6 +259,33 @@ if (DCK_mon_start_cnt >= DCK_mon_start_per) {
 				DROPP_LOAD_CTRL(EpD[SET_DROPPER_K2][0].V1);
 				apply_state_changes_f(DROPPER2_BYP_FC, EpD[SET_DROPPER_K2][0].V1);
 			}
+		} else if (VLOAD_pas.a1+dropper_test_var_1 < Vdc_drop_out_min && !is_state_active(DROPPER1_BYP_FC) && !is_state_active(DROPPER2_BYP_FC)) {
+
+
+			if (VLOAD_pas.a1 < VLOAD_DC_LOW_LIM && !is_state_active(LOAD_DC_LW_FC)) {
+				VLOAD_DC_LOW_LIM_Acc_cnt++;
+				VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+				if (VLOAD_DC_LOW_LIM_Acc_cnt >= VLOAD_DC_LOW_LIM_Acc_per) {
+					VLOAD_DC_LOW_LIM_Acc_cnt=0;
+					apply_state_changes_f(LOAD_DC_LW_FC, 1);
+					sprintf(DUB,"LOAD DC Low"); prfm(DUB);
+				}
+			} else {
+				VLOAD_DC_LOW_LIM_Acc_cnt=0;
+			}
+			if (VLOAD_pas.a1 >= VLOAD_DC_LOW_LIM_ret && is_state_active(LOAD_DC_LW_FC)) {
+				VLOAD_DC_LOW_LIM_ret_Acc_cnt++;
+				VLOAD_DC_LOW_LIM_Acc_cnt=0;
+				if (VLOAD_DC_LOW_LIM_ret_Acc_cnt >= VLOAD_DC_LOW_LIM_ret_Acc_per) {
+					VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+					apply_state_changes_f(LOAD_DC_LW_FC, 0);
+					sprintf(DUB,"LOAD DC Low Return"); prfm(DUB);
+				}
+			} else {
+				VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+			}
+
+
 		}
 
 ////// MANAGE DROPPER ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -288,28 +315,28 @@ if (VLOAD_pas.a1 <= VLOAD_DC_HIGH_LIM_ret && is_state_active(LOAD_DC_HG_FC)) {
 } else {
 	VLOAD_DC_HIGH_LIM_ret_Acc_cnt=0;
 }
-if (VLOAD_pas.a1 < VLOAD_DC_LOW_LIM && !is_state_active(LOAD_DC_LW_FC)) {
-	VLOAD_DC_LOW_LIM_Acc_cnt++;
-	VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
-	if (VLOAD_DC_LOW_LIM_Acc_cnt >= VLOAD_DC_LOW_LIM_Acc_per) {
-		VLOAD_DC_LOW_LIM_Acc_cnt=0;
-		apply_state_changes_f(LOAD_DC_LW_FC, 1);
-		sprintf(DUB,"LOAD DC Low"); prfm(DUB);
-	}
-} else {
-	VLOAD_DC_LOW_LIM_Acc_cnt=0;
-}
-if (VLOAD_pas.a1 >= VLOAD_DC_LOW_LIM_ret && is_state_active(LOAD_DC_LW_FC)) {
-	VLOAD_DC_LOW_LIM_ret_Acc_cnt++;
-	VLOAD_DC_LOW_LIM_Acc_cnt=0;
-	if (VLOAD_DC_LOW_LIM_ret_Acc_cnt >= VLOAD_DC_LOW_LIM_ret_Acc_per) {
-		VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
-		apply_state_changes_f(LOAD_DC_LW_FC, 0);
-		sprintf(DUB,"LOAD DC Low Return"); prfm(DUB);
-	}
-} else {
-	VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
-}
+//if (VLOAD_pas.a1 < VLOAD_DC_LOW_LIM && !is_state_active(LOAD_DC_LW_FC)) {
+//	VLOAD_DC_LOW_LIM_Acc_cnt++;
+//	VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+//	if (VLOAD_DC_LOW_LIM_Acc_cnt >= VLOAD_DC_LOW_LIM_Acc_per) {
+//		VLOAD_DC_LOW_LIM_Acc_cnt=0;
+//		apply_state_changes_f(LOAD_DC_LW_FC, 1);
+//		sprintf(DUB,"LOAD DC Low"); prfm(DUB);
+//	}
+//} else {
+//	VLOAD_DC_LOW_LIM_Acc_cnt=0;
+//}
+//if (VLOAD_pas.a1 >= VLOAD_DC_LOW_LIM_ret && is_state_active(LOAD_DC_LW_FC)) {
+//	VLOAD_DC_LOW_LIM_ret_Acc_cnt++;
+//	VLOAD_DC_LOW_LIM_Acc_cnt=0;
+//	if (VLOAD_DC_LOW_LIM_ret_Acc_cnt >= VLOAD_DC_LOW_LIM_ret_Acc_per) {
+//		VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+//		apply_state_changes_f(LOAD_DC_LW_FC, 0);
+//		sprintf(DUB,"LOAD DC Low Return"); prfm(DUB);
+//	}
+//} else {
+//	VLOAD_DC_LOW_LIM_ret_Acc_cnt=0;
+//}
 ////// V LOAD DC HIGH/LOW MON ///////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
