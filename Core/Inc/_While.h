25b0a4f5645ace969e788824335218aba443db8a
tuncel3 <tuncel3@gmail.com>
2025-04-26 05:15:39 +0300

    Auto commit on 2025-04-26 05:15 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -805,7 +805,7 @@ if (sfsta_op_phase == S_SFSTA_REQ_OK) {
 	if (!SW_BATT_OFF && VBAT_pas.a16 > Vbat_flt && !batt_current_detected && blm_op_phase==0) {
 		blm_op_phase=1;
 	}
-	if (blm_op_phase == 1 && EpD[SET_BATT_DISC_DET][0].V1==1 && vrect_stable) {
+	if (blm_op_phase == 1 && EpD[SET_BATT_DISC_DET][0].V1==1 && vrect_stable) {		// BATT MONITORING ENABLED OR DISABLED
 		PRF_GEN("vrect stable. ");
 		blm_op_phase=2;
 	} else if (blm_op_phase == 2) { // Başlatma. vrect stable değilse başlama. sakin durumda iken yap.
