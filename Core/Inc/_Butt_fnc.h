b31f45712665e8901034eea8b4600cee9b09879c
tuncel3 <tuncel3@gmail.com>
2025-04-27 18:39:58 +0300

    Auto commit on 2025-04-27 18:39 - auto

--- Core/Inc/_Butt_fnc.h
+++ Core/Inc/_Butt_fnc.h
@@ -487,13 +487,13 @@ if (!chg_setting_edit_mode) {
     	if (dropper_edit_mode == 0) {
     		selected_DROPPER_PG_line=(selected_DROPPER_PG_line-1+3) % 3;
 		} else if (dropper_edit_mode == 1) {
-			if (selected_DROPPER_PG_line == 0) {
-				EpD[SET_DROPPER_MANOTO][1].V1^=1;
-			} else if (selected_DROPPER_PG_line == 1) {
-				EpD[SET_DROPPER_K1][1].V1^=1;
-			} else if (selected_DROPPER_PG_line == 2) {
-				EpD[SET_DROPPER_K2][1].V1^=1;
-			}
+	        if (selected_DROPPER_PG_line == 0) {
+	            EpD[SET_DROPPER_MANOTO][1].V1 = (EpD[SET_DROPPER_MANOTO][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        } else if (selected_DROPPER_PG_line == 1) {
+	            EpD[SET_DROPPER_K1][1].V1 = (EpD[SET_DROPPER_K1][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        } else if (selected_DROPPER_PG_line == 2) {
+	            EpD[SET_DROPPER_K2][1].V1 = (EpD[SET_DROPPER_K2][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        }
 		}
     }
 
@@ -842,13 +842,13 @@ if (!chg_setting_edit_mode) {
 		if (dropper_edit_mode == 0) {
 			selected_DROPPER_PG_line=(selected_DROPPER_PG_line+1+3) % 3;
 		} else if (dropper_edit_mode == 1) {
-			if (selected_DROPPER_PG_line == 0) {
-				EpD[SET_DROPPER_MANOTO][1].V1^=1;
-			} else if (selected_DROPPER_PG_line == 1) {
-				EpD[SET_DROPPER_K1][1].V1^=1;
-			} else if (selected_DROPPER_PG_line == 2) {
-				EpD[SET_DROPPER_K2][1].V1^=1;
-			}
+	        if (selected_DROPPER_PG_line == 0) {
+	            EpD[SET_DROPPER_MANOTO][1].V1 = (EpD[SET_DROPPER_MANOTO][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        } else if (selected_DROPPER_PG_line == 1) {
+	            EpD[SET_DROPPER_K1][1].V1 = (EpD[SET_DROPPER_K1][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        } else if (selected_DROPPER_PG_line == 2) {
+	            EpD[SET_DROPPER_K2][1].V1 = (EpD[SET_DROPPER_K2][1].V1 == 0.0f) ? 1.0f : 0.0f;
+	        }
 		}
 	}
 
