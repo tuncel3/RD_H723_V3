e6d1fc56d952125a7c7f3b56d258b4b14a216ea0
tuncel3 <tuncel3@gmail.com>
2025-04-28 23:20:22 +0300

    Auto commit on 2025-04-28 23:20 - auto

--- Core/Inc/_Butt_fnc.h
+++ Core/Inc/_Butt_fnc.h
@@ -354,13 +354,12 @@ void bup_fnc(void) {
     	asm("NOP");
     }
     else if (currentPage == MAIN_MENU_pg) {
-    	selected_MAIN_MENU=(selected_MAIN_MENU-1+NUM_MAIN_MENU_ITEMS) % NUM_MAIN_MENU_ITEMS;
 		if (main_menu_arrow_loc > 1) {
 			main_menu_arrow_loc--;
 		} else if (main_menu_arrow_loc == 1) {
 			main_menu_disp_index=(main_menu_disp_index-1+NUM_MAIN_MENU_ITEMS) % NUM_MAIN_MENU_ITEMS;
 		}
-//		selected_DEVICE_SETT=(main_menu_disp_index+main_menu_arrow_loc) % NUM_MAIN_MENU_ITEMS;
+    	selected_MAIN_MENU=(selected_MAIN_MENU-1+NUM_MAIN_MENU_ITEMS) % NUM_MAIN_MENU_ITEMS;
     }
     else if (currentPage == DEVICE_SETT_pg) {
 
@@ -731,7 +730,12 @@ void bdown_fnc(void) {
     	HOME_PAGE_pg_sel = 1;
     }
     else if (currentPage == MAIN_MENU_pg) {
-    	selected_MAIN_MENU=(selected_MAIN_MENU+1) % NUM_MAIN_MENU_ITEMS;
+		if (main_menu_arrow_loc > 1) {
+			main_menu_arrow_loc--;
+		} else if (main_menu_arrow_loc == 1) {
+			main_menu_disp_index=(main_menu_disp_index+1+NUM_MAIN_MENU_ITEMS) % NUM_MAIN_MENU_ITEMS;
+		}
+    	selected_MAIN_MENU=(selected_MAIN_MENU+1+NUM_MAIN_MENU_ITEMS) % NUM_MAIN_MENU_ITEMS;
     }
     else if (currentPage == DEVICE_SETT_pg) {
 
