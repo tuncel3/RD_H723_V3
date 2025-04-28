073c2666258c1f25ed514ee8119acdaf011efb79
tuncel3 <tuncel3@gmail.com>
2025-04-28 15:20:15 +0300

    Auto commit on 2025-04-28 15:20 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -745,10 +745,10 @@ void inline extern set_variables_from_EEP_fc(uint8_t scope) { // n012
     	//if (scope==dropper_limits) {
     	//Vdc_drop_in_min=EpD[DEV_NOM_VOUT][0].V1*0.9; // D.A. gerilim regülasyonu giriş gerilimi
     	//Vdc_drop_in_max=EpD[DEV_NOM_VOUT][0].V1*1.3; // D.A. gerilim regülasyonu giriş gerilimi
-//        dropp_reg_high_lim = EpD[DEV_NOM_VOUT][0].V1 * (1 + (EpD[dropp_reg_high_lim_add][0].V1 / 100));
-//        dropp_reg_low_lim  = EpD[DEV_NOM_VOUT][0].V1 * (1 - (EpD[dropp_reg_low_lim_sub][0].V1 / 100));
+        set_dropper_l_hg = EpD[DEV_NOM_VOUT][0].V1 * (1 + (EpD[SET_DROPPER_L_HG][0].V1 / 100));
+        set_dropper_l_lw  = EpD[DEV_NOM_VOUT][0].V1 * (1 - (EpD[SET_DROPPER_L_LW][0].V1 / 100));
     	set_dropper_l_hg=EpD[SET_DROPPER_L_HG][0].V1;
-    	set_dropper_l_lw=EpD[SET_DROPPER_L_LW][1].V1;
+    	set_dropper_l_lw=EpD[SET_DROPPER_L_LW][0].V1;
     }
     if (scope & SCOPE_CURRENT_LIMITS_FROM_EEP || scope == SCOPE_VAR_ALL_FROM_EEP) {
         Irect_max = EpD[DEV_NOM_IOUT][0].V1 * 1.0;
