966c01bd89a5b2f7465c7bc4c4d386a210f2d8d2
tuncel3 <tuncel3@gmail.com>
2025-04-29 12:02:21 +0300

    Auto commit on 2025-04-29 12:02 - auto

--- Core/Inc/_LCD_Pg.h
+++ Core/Inc/_LCD_Pg.h
@@ -14,7 +14,7 @@ inline extern void FANS_TEMP_pg_disp(void) {
 
 	if (fan_temp_edit_mode) {
 		if ((fan_temp_blink^=1)==0) {
-			x0=64+(fan_temp_dig*6); y0=(selected_FAN_TEMP_PG_line+2)*lnhg-1; w=4;
+			x0=95+(fan_temp_dig*6); y0=(selected_FAN_TEMP_PG_line+2)*lnhg-1; w=4;
 		}
 	}
 	GLCD_Line(x0, y0, x0+w, y0);
