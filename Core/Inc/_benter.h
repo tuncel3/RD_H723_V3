5c9dfc323fb6e348c34eff21f17893aabae28fa7
tuncel3 <tuncel3@gmail.com>
2025-04-30 00:32:32 +0300

    Auto commit on 2025-04-30 00:32 - auto

--- Core/Inc/_benter.h
+++ Core/Inc/_benter.h
@@ -135,15 +135,6 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_BATT_DISC_DET) {
 else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==DEV_NOM_VOUT) {
 	EpD[DEV_NOM_VOUT][1].V1=EpD[DEV_NOM_VOUT][0].V1;
 }
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_ALRM_LIM) {
-//	EpD[SET_OVTM_ALRM_LIM][1].V1=EpD[SET_OVTM_ALRM_LIM][0].V1;
-//}
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_OPEN_DUR) {
-//	EpD[SET_OVTM_OPEN_DUR][1].V1=EpD[SET_OVTM_OPEN_DUR][0].V1;
-//}
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_OPEN_LIM) {
-//	EpD[SET_OVTM_OPEN_LIM][1].V1=EpD[SET_OVTM_OPEN_LIM][0].V1;
-//}
 else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==IRECT_LIM_RT_) {
 	EpD[IRECT_LIM_RT_][1].V1=EpD[IRECT_LIM_RT_][0].V1;
 }
@@ -183,18 +174,6 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==DEV_NOM_VOUT) {
 	set_variables_from_EEP_fc(SCOPE_DEV_NOM_VOUT_EEP);
 	Rec_Dat_to_EEp_f(DEV_NOM_VOUT);
 }
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_ALRM_LIM) {
-//	EpD[SET_OVTM_ALRM_LIM][0].V1=EpD[SET_OVTM_ALRM_LIM][1].V1;
-//	Rec_Dat_to_EEp_f(SET_OVTM_ALRM_LIM);
-//}
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_OPEN_DUR) {
-//	EpD[SET_OVTM_OPEN_DUR][0].V1=EpD[SET_OVTM_OPEN_DUR][1].V1;
-//	Rec_Dat_to_EEp_f(SET_OVTM_OPEN_DUR);
-//}
-//else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==SET_OVTM_OPEN_LIM) {
-//	EpD[SET_OVTM_OPEN_LIM][0].V1=EpD[SET_OVTM_OPEN_LIM][1].V1;
-//	Rec_Dat_to_EEp_f(SET_OVTM_OPEN_LIM);
-//}
 else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==IRECT_LIM_RT_) {
 	EpD[IRECT_LIM_RT_][0].V1=EpD[IRECT_LIM_RT_][1].V1;
 	Rec_Dat_to_EEp_f(IRECT_LIM_RT_);
