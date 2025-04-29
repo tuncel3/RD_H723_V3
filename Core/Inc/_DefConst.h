152d7b75037fbd53a705465a89ff7e7da63d0b0a
tuncel3 <tuncel3@gmail.com>
2025-04-29 16:53:26 +0300

    Auto commit on 2025-04-29 16:53 - auto

--- Core/Inc/_DefConst.h
+++ Core/Inc/_DefConst.h
@@ -345,7 +345,7 @@ const char* FANS_TEMP_Items[] = {
 uint8_t fan_temp_edit_mode = 0;
 uint8_t selected_FANS_TEMP = 0;
 uint8_t selected_FAN_TEMP_PG_line = 0;
-uint8_t fan_temp_dig = 2;
+uint8_t fan_temp_dig = 1;
 uint8_t fan_temp_edit_blink = 0;
 
 const char* MAIN_MENU_Items[] = {
