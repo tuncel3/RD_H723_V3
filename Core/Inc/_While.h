0a3143a6dcf67fadc87db674aaff0ecb2ff6db5f
tuncel3 <tuncel3@gmail.com>
2025-04-30 21:52:16 +0300

    Auto commit on 2025-04-30 21:52 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -1097,7 +1097,7 @@ if (leds_rels_override && leds_rels_override_returned) {
 	LED_7_Data=255;
 	REL_24Bit_Data=16777215;
 	leds_rels_override_returned=0;
-} else if (!leds_rels_override_returned) {
+} else if (!leds_rels_override && !leds_rels_override_returned) {
 	leds_rels_override_returned=1;
 	LED_16_Data=LED_16_Data_h;
 	LED_7_Data=LED_7_Data_h;
