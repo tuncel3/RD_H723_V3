d4506911b108c57007cdd2f374f091a71b189402
tuncel3 <tuncel3@gmail.com>
2025-04-30 09:10:07 +0300

    Auto commit on 2025-04-30 09:10 - auto

--- Core/Inc/_Main.h
+++ Core/Inc/_Main.h
@@ -171,20 +171,19 @@ if (EpD[RECT_ACTV_AT_STARTUP][0].V1==1) {
 // aşırı sıcaklık alarm
 // aşırı sıcaklık açma, gecikme süresi ile
 // zero cross var yok flagı lazım. yoksa ac voltaj ölçümü geçersiz durumda oluyor be lcd de gösterilmemeli. 0 olarak gösterilmeli.
-// 3 - butona basar basmaz tepki olmuyor. bunu hızlandırmak lazım. butonu bırakınca kısa süreliğine butonu disable etmek lazım.
-// 1 - data kayıt için adrese ihtiyaç yok
-// 6 - arıza tekrarladıkça geri dönme süresini uzatma algoritması
 // 4 - eeprom okuma başarısız ise arıza durumu gösterilmeli.
 // 3 - bir anda batarya akımı kesilirse battery inspection hemen başlat.
 // 5 - blm bat bağlı, rect akım sınırı 3.4 ve bat akımı 0 iken blm sistemi kararsız çalışıyor. bat inceleme yöntemi kullanıldı sonuç alınamadığında başka bir yöntem kullanılmalı. mesela hızlı yöntem olan korelasyon yerine yavaş yavaş düşürerek takip edip etmediğine bakma.
 // batarya devreye alındığında DC short hatası oluyor. DC short hatası olmadan batarya devreye alınmalı.
 // bunu yapmak için kısa devre akımındaki değişime bakılabilir. aşağı yönlü bir değişim varsa bunu batarya devreye alma olarak algıla.
 // eeprom kaydetemden önce oku. aynı değer varsa yazma.
+// 6 - arıza tekrarladıkça geri dönme süresini uzatma algoritması
 // çıkış voltajı yokken. yani ne doğrultucu ne de batarya dc vermiyorsa çıkışa, dc kaçak yüzdesi yüksek olabiliyor.
 // böyle bir durumda yanlış dck oluşmaması için çıkış voltajı nominal voltajın %20 altında ve çıkış, doğrultucu ve
 // batarya akımı yoksa dck yüzdesini önemseme
 // rectifier kapalı iken, akü şalter devre dışı iken kaçak voltaj arızası oluşması engellenmeli.
 //short circuit detection için. short circuit akımındaki değişime bakılabilir.
+// 1 - data kayıt için adrese ihtiyaç yok
 
 
 
