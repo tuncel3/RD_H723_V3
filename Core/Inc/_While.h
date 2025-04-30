f16f893801a4a4f957ff9c546bfcfa0860b36748
tuncel3 <tuncel3@gmail.com>
2025-04-30 15:04:11 +0300

    Auto commit on 2025-04-30 15:04 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -695,6 +695,7 @@ if ((VAC_R_Lo_fc == 0 && VAC_S_Lo_fc == 0 && VAC_T_Lo_fc == 0) && is_state_activ
 		} else if (tmp_dat_C[0]+temp_test_thy_1 < EpD[SET_OVERTEMP_ALARM][0].V1-5 && is_state_active(OVERTEMP_ALARM_FC)) { // overtemp alarm disable
 			apply_state_changes_f(OVERTEMP_ALARM_FC, 0);
 		}
+
 		if (tmp_dat_C[0]+temp_test_thy_1 > EpD[SET_OVERTEMP_OPEN][0].V1 && !is_state_active(OVERTEMP_OPEN_FC)) { // overtemp open count
 			ovtmp_open_cnt++;
 		} else if (tmp_dat_C[0]+temp_test_thy_1 < EpD[SET_OVERTEMP_OPEN][0].V1-5 && is_state_active(OVERTEMP_OPEN_FC)) { // overtemp open cancel
@@ -712,14 +713,12 @@ if ((VAC_R_Lo_fc == 0 && VAC_S_Lo_fc == 0 && VAC_T_Lo_fc == 0) && is_state_activ
 		} else if (tmp_dat_C[0]+temp_test_thy_1 < EpD[SET_COOL_FAN_TEMP][0].V1-5 && is_state_active(THY_FAN1_REL)) { // fan disable
 			apply_state_changes_f(THY_FAN1_REL, 0);
 		}
+
 		if (tmp_dat_C[1]+temp_test_trf_2 > EpD[SET_TRANSF_FAN_TEMP][0].V1 && !is_state_active(TRF_FAN2_REL)) { // fan enable
 			apply_state_changes_f(TRF_FAN2_REL, 1);
 		} else if (tmp_dat_C[0]+temp_test_trf_2 < EpD[SET_TRANSF_FAN_TEMP][0].V1-5 && is_state_active(TRF_FAN2_REL)) { // fan disable
 			apply_state_changes_f(TRF_FAN2_REL, 0);
 		}
-
-
-
 	}
 
 //// AKÜ ////////////////////////////////////////////////////////////////////////////////////////////////////////////
