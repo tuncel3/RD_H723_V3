bf599a19ff6ee2c0110973b57953487fbdcc9841
tuncel3 <tuncel3@gmail.com>
2025-04-30 20:55:15 +0300

    Auto commit on 2025-04-30 20:55 - auto

--- Core/Inc/_benter.h
+++ Core/Inc/_benter.h
@@ -394,6 +394,12 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==VRECT_DC_LOW_LIM_add) {
 			if (selected_TEST_PG_line==1) {
 				temp_test_trf_2=0;
 			}
+			if (selected_TEST_PG_line==2) {
+				buzzer_override=1;
+			}
+			if (selected_TEST_PG_line==3) {
+				leds_override=1;
+			}
         }
         if (!test_edit_mode) {
         	__NOP();
