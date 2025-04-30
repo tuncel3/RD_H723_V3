aef48df8b11b13887911e8dc13ae6c08a157122a
tuncel3 <tuncel3@gmail.com>
2025-04-30 11:25:09 +0300

    Auto commit on 2025-04-30 11:25 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -695,6 +695,11 @@ if ((VAC_R_Lo_fc == 0 && VAC_S_Lo_fc == 0 && VAC_T_Lo_fc == 0) && is_state_activ
 		} else if (tmp_dat_C[0]+temp_test_var_1 < EpD[SET_OVERTEMP_ALARM][0].V1-5 && is_state_active(OVERTEMP_ALARM_FC)) { // overtemp alarm disable
 			apply_state_changes_f(OVERTEMP_ALARM_FC, 0);
 		}
+		if (tmp_dat_C[0]+temp_test_var_1 > EpD[SET_OVERTEMP_OPEN][0].V1 && !is_state_active(OVERTEMP_ALARM_FC)) { // overtemp alarm enable
+			apply_state_changes_f(OVERTEMP_ALARM_FC, 1);
+		} else if (tmp_dat_C[0]+temp_test_var_1 < EpD[SET_OVERTEMP_OPEN][0].V1-5 && is_state_active(OVERTEMP_ALARM_FC)) { // overtemp alarm disable
+			apply_state_changes_f(OVERTEMP_ALARM_FC, 0);
+		}
 
 //			ovtmp_open_cnt=0; // reset open count only when alarm is deactivated
 //		else if (tmp_dat_C[0]+temp_test_var_1 > EpD[SET_OVERTEMP_OPEN][0].V1 && !is_state_active(SET_OVERTEMP_OPEN)) { // overtemp open enable
