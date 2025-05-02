7463a30d06887a3ee0ed0742f77cf5ef23dcdf4e
tuncel3 <tuncel3@gmail.com>
2025-05-02 06:22:39 +0300

    Auto commit on 2025-05-02 06:22 - auto

--- Core/Inc/_Fnc_IRQ.h
+++ Core/Inc/_Fnc_IRQ.h
@@ -373,16 +373,17 @@ void TIM7_IRQHandler(void)
 
 		if (zero_cross_timeout_R > 500) {
 			zero_cross_timeout_R=501;
-			reset_RMS_val_R=1;
+			no_zero_cross_R=1;
 			VAC_R_rms_roll_per_avg.a64=0;	// zero cross olmadığı için takılı kalmış olan RMS değeri sıfırlanmalı.
 		}
 		if (zero_cross_timeout_S > 500) {
 			zero_cross_timeout_S=501;
-			reset_RMS_val_S=1;
+			no_zero_cross_S=1;
 			VAC_S_rms_roll_per_avg.a64=0;	// zero cross olmadığı için takılı kalmış olan RMS değeri sıfırlanmalı.
 		}
 		if (zero_cross_timeout_T > 500) {
 			zero_cross_timeout_T=501;
+			no_zero_cross_T=1;
 			VAC_T_rms_roll_per_avg.a64=0;	// zero cross olmadığı için takılı kalmış olan RMS değeri sıfırlanmalı.
 		}
 	}
