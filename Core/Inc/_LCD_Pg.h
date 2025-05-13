77cd570be6c669fdf9d644f07065a939809be72c
tuncel3 <tuncel3@gmail.com>
2025-05-13 10:35:00 +0300

    Auto commit on 2025-05-13 10:35 - auto

--- Core/Inc/_LCD_Pg.h
+++ Core/Inc/_LCD_Pg.h
@@ -267,14 +267,13 @@ if (temp_sens_count==2) {
 
 ///////////////////////////////////////////////////////////////
 
-	for (tabl_dolas = son_kal; tabl_dolas < NUM_STATE_NAMES; tabl_dolas++) {
-		if (state_list[tabl_dolas].action & (1 << LCD_roll_enum) && state_list[tabl_dolas].action & (1 << ACTIVE_enum)) {
-			sprintf(M, "%s", state_list[tabl_dolas].name); GLCD_PrintString(0, 0, M);
-			son_kal=tabl_dolas+1;
-			break;
-		}
+for (tabl_dolas = son_kal; tabl_dolas < NUM_STATE_NAMES; tabl_dolas++) {
+	if (state_list[tabl_dolas].action & (1 << LCD_roll_enum) && state_list[tabl_dolas].action & (1 << ACTIVE_enum)) {
+		sprintf(M, "%s", state_list[tabl_dolas].name); GLCD_PrintString(0, 0, M);
+		son_kal=tabl_dolas+1;
+		break;
 	}
-
+}
 if (tabl_dolas == NUM_STATE_NAMES) {
 	tabl_dolas=0;
 	son_kal=0;
