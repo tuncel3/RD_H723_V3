ec9b6b0da3b41bdf18af8cd7081d79b10b904371
tuncel3 <tuncel3@gmail.com>
2025-05-20 14:45:41 +0300

    Auto commit on 2025-05-20 14:45 - auto

--- Core/Inc/_LCD_Pg.h
+++ Core/Inc/_LCD_Pg.h
@@ -272,7 +272,7 @@ if (tabl_dolas_delay_cnt==0) {
 		tabl_dolas=0;
 		son_kal=0;
 
-		for (tabl_dolas = son_kal; tabl_dolas < NUM_STATE_NAMES; tabl_dolas++) { // üstteki for döngüsü ile aynı. yazma işlemi üstteki loopta yapılıyor.
+		for (tabl_dolas = son_kal; tabl_dolas < NUM_STATE_NAMES; tabl_dolas++) { // üstteki for döngüsü ile aynı. yazma işlemi üstteki loopta yapılıyor. üstteki loop bir şey bulamaıp tablo sonuna geldi diyelim. bu durumda birşey bulunamadığı için bu döngüde boş satır yazılacak. bunu engellemek için aynı loop buraya da koyuluyor.
 			if (state_list[tabl_dolas].action & (1 << LCD_roll_enum) && state_list[tabl_dolas].action & (1 << ACTIVE_enum)) {
 				sprintf(RollBuf, "%s", state_list[tabl_dolas].name);
 				son_kal=tabl_dolas+1;
