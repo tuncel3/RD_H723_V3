0be585d85766d68ed95370301e8fee9e34749305
tuncel3 <tuncel3@gmail.com>
2025-05-30 15:18:49 +0300

    Restored from 5d04f8b and saved as new backup on Cum 30.05.2025 15:18:48.96

--- Core/Inc/_benter.h
+++ Core/Inc/_benter.h
@@ -321,6 +321,17 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==VRECT_DC_LOW_LIM_add) {
 			currentPage = TEST_pg;
 		}
     }
+    else if (currentPage == CALIBRATION_2_pg) {
+    	cal_pg2_sel_edit_mode=(cal_pg2_sel_edit_mode+1)%3;
+
+		if (cal_pg2_sel_edit_mode == cal_offset) { // yani enter a basılmış ve gain den offset edit moduna geçilmiş. yani gain değerinin kaydedilmesi lazım.
+			if (cal_pg2_sel_col==0) {
+				if (cal_pg2_sel_item_left==0) {
+					Rec_Dat_to_EEp_f(SET_VRECT_CAL);
+				}
+
+			}
+    }
     else if (currentPage == CALIBRATION_1_pg) {
     	cal_pg1_sel_edit_mode=(cal_pg1_sel_edit_mode+1)%3;
 
