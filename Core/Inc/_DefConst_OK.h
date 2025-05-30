e3a7aa261b08f92337010b98c23da31ef70fe188
tuncel3 <tuncel3@gmail.com>
2025-05-30 20:03:07 +0300

    Auto commit on 2025-05-30 20:03 - auto

--- Core/Inc/_DefConst_OK.h
+++ Core/Inc/_DefConst_OK.h
@@ -243,6 +243,7 @@ uint32_t reset_RMS_val_T = 0;
 uint32_t rms_zc_for_DC_cnt = 0;
 uint32_t rms_zc_for_DC = 0;
 
+floatfrq_rst_updn_avg_m=0;
 
 int32_t per_r_dn_smp = 0;
 uint32_t per_r_dn_avg_sum = 0;
