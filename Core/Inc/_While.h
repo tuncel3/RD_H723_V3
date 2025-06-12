a14ff9ee80a98839577cef667e0341239ee43460
unknown <tuncel3@gmail.com>
2025-06-12 15:14:45 +0300

    Auto commit on 2025-06-12 15:14 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -994,15 +994,15 @@ if (sfsta_op_phase == S_SFSTA_REQ_OK) {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 
-alpha_rad =  (timx_trg_num * (M_PI / (tim_arr_max+tim_arr_max_c)));
-factor = (M_PI - alpha_rad + 0.5f * sinf(2.0f * alpha_rad)) / M_PI;
-i_ac_rms = (IRECT_pas.a64 / sqrtf(3.0f)) * sqrtf(factor) * verim;
+//alpha_rad =  (timx_trg_num * (M_PI / (tim_arr_max+tim_arr_max_c)));
+//factor = (M_PI - alpha_rad + 0.5f * sinf(2.0f * alpha_rad)) / M_PI;
+//i_ac_rms = (IRECT_pas.a64 / sqrtf(3.0f)) * sqrtf(factor) * verim;
 
 
-power_out=VRECT_pas.a64*IRECT_pas.a64;
+power_out=VRECT_pas.a1*IRECT_pas.a1;
 power_in=power_out/verim;
 IAC_R_rms_sc.a64=power_in/VAC_S_rms_sc.a64;
-timx_rat= (float) timx_trg_num/tim_arr_max;
+timx_rat=  ((float) (timx_trg_num+zc_start_delay_300u_arr_32))/tim_arr_max;
 
 
 
