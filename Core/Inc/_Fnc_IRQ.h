67b7d95fc7f123945ccedfb7d0ca62c029ce7a97
unknown <tuncel3@gmail.com>
2025-06-12 11:49:02 +0300

    Auto commit on 2025-06-12 11:49 - auto

--- Core/Inc/_Fnc_IRQ.h
+++ Core/Inc/_Fnc_IRQ.h
@@ -268,6 +268,7 @@ void TIM3_IRQHandler(void) {
 void EXTI9_5_IRQHandler(void){
   if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_7) != RESET)  {
 		LL_TIM_DisableCounter(TIM2);
+		TIM2_stopped_at=LL_TIM_GetCounter(TIM2);
 		LL_TIM_SetCounter(TIM2, 0);
 		LL_TIM_SetCounter(TIM3, 0);
 		LL_TIM_EnableCounter(TIM3);
@@ -292,6 +293,7 @@ void EXTI9_5_IRQHandler(void){
   }
   if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_8) != RESET)  {
 		LL_TIM_DisableCounter(TIM23);
+		TIM23_stopped_at=LL_TIM_GetCounter(TIM23);
 		LL_TIM_SetCounter(TIM23, 0);
 		LL_TIM_SetCounter(TIM3, 0);
 		LL_TIM_EnableCounter(TIM3);
@@ -315,6 +317,7 @@ void EXTI9_5_IRQHandler(void){
   }
   if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_9) != RESET)  {
 		LL_TIM_DisableCounter(TIM24);
+		TIM24_stopped_at=LL_TIM_GetCounter(TIM24);
 		LL_TIM_SetCounter(TIM24, 0);
 		LL_TIM_SetCounter(TIM3, 0);
 		LL_TIM_EnableCounter(TIM3);
