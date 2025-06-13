4850b8b3f20f74aa32d487fbc28e08f90b23e74a
unknown <tuncel3@gmail.com>
2025-06-13 15:38:31 +0300

    Auto commit on 2025-06-13 15:38 - auto

--- Core/Inc/_LCD_Pg.h
+++ Core/Inc/_LCD_Pg.h
@@ -530,7 +530,7 @@ inline extern void MAIN_MENU_pg_disp(void) {
 
     for (uint8_t i = 0; i < 6; i++) {
 		sprintf(L, " %s ", MAIN_MENU_Items[main_menu_disp_index_]);
-    	if (main_menu_disp_index_!=6 && secret_menu_cnt) {
+    	if (main_menu_disp_index_!=6 && secret_menu_en) {
     		GLCD_PrintString(0, (i + 1) * 9, L);
     	}
 		GLCD_PrintString(0, (main_menu_arrow_loc+1) * 9, ">");
