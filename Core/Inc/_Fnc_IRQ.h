07cd69172b60d1aa9c4996133860a546fceccbbd
unknown <tuncel3@gmail.com>
2025-06-19 19:28:14 +0300

    Auto commit on 2025-06-19 19:28 - auto

--- Core/Inc/_Fnc_IRQ.h
+++ Core/Inc/_Fnc_IRQ.h
@@ -432,6 +432,24 @@ void SysTick_Handler(void) {	// n009
 			LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
 		}
 	}
+	if (LL_EXTI_LINE_8_rise_reenable_cnt < zcrendely_S_rise) {
+		LL_EXTI_LINE_8_rise_reenable_cnt++;
+		if (LL_EXTI_LINE_8_rise_reenable_cnt == zcrendely_R_rise && LL_EXTI_IsEnabledRisingTrig_0_31(LL_EXTI_LINE_8)) {
+			LL_EXTI_LINE_8_rise_reenable_cnt=zcrendely_R_rise+1;
+			LL_EXTI_DisableRisingTrig_0_31(LL_EXTI_LINE_7);
+			LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_7);
+			LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
+		}
+	}
+	if (LL_EXTI_LINE_8_rise_reenable_cnt < zcrendely_S_rise) {
+		LL_EXTI_LINE_8_rise_reenable_cnt++;
+		if (LL_EXTI_LINE_8_fall_reenable_cnt == zcrendely_R_fall && LL_EXTI_IsEnabledFallingTrig_0_31(LL_EXTI_LINE_8)) {
+			LL_EXTI_LINE_8_fall_reenable_cnt=zcrendely_R_fall+1;
+			LL_EXTI_DisableFallingTrig_0_31(LL_EXTI_LINE_7);
+			LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_7);
+			LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
+		}
+	}
 	if (LL_EXTI_LINE_8_reenable_cnt < ZCRENDELY) {
 		LL_EXTI_LINE_8_reenable_cnt++;
 		if (LL_EXTI_LINE_8_reenable_cnt == ZCRENDELY) {
