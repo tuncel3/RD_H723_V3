a41f534cbf6be2ae392d9f9d0ca7d4d9d365267b
unknown <tuncel3@gmail.com>
2025-06-19 16:22:11 +0300

    Auto commit on 2025-06-19 16:22 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -616,9 +616,6 @@ if ((VAC_R_Lo_fc == 0 && VAC_S_Lo_fc == 0 && VAC_T_Lo_fc == 0) && is_state_activ
 
 // FREQUENCY (50ms loop)
 	frq_cal_k=275e6*EpD[SET_FRQ_CAL][0].V1;
-//	frq_r_updn_avg_m=frq_cal_k / (float) per_r_updn_avg_m;
-//	frq_s_updn_avg_m=frq_cal_k / (float) per_s_updn_avg_m;
-//	frq_t_updn_avg_m=frq_cal_k / (float) per_t_updn_avg_m;
 	frq_rst_updn_avg_m=frq_cal_k*3 / (float) (per_r_updn_avg_m + per_s_updn_avg_m + per_t_updn_avg_m);
 	zcrendely_R_U=((1/frq_cal_k)*1e-6*per_r_dn_smp);
 
