c73a88188bf5e48c78b3ff2fa5aeeb43c1ee1875
unknown <tuncel3@gmail.com>
2025-06-20 14:42:38 +0300

    Auto commit on 2025-06-20 14:42 - auto

--- Core/Inc/_Fnc_IRQ.h
+++ Core/Inc/_Fnc_IRQ.h
@@ -428,7 +428,7 @@ void SysTick_Handler(void) {	// n009
 		if (exti_7R_fall_en_cnt < exti_7R_fall_en_per) {	    // saymaya devam et
 			exti_7R_fall_en_cnt++;
 			if (exti_7R_fall_en_cnt == exti_7R_fall_en_per) {   // sayma tamam
-				exti_7R_fall_en_cnt=exti_7R_fall_en_per << 3;
+				exti_7R_fall_en_cnt=99999;
 				LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_7); // falling enable et
 				LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
 			}
@@ -438,7 +438,7 @@ void SysTick_Handler(void) {	// n009
 		if (exti_7R_rise_en_cnt < exti_7R_rise_en_per) {
 			exti_7R_rise_en_cnt++;
 			if (exti_7R_rise_en_cnt == exti_7R_rise_en_per) {
-				exti_7R_rise_en_cnt=exti_7R_rise_en_per << 3;
+				exti_7R_rise_en_cnt=99999;
 				LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_7);
 				LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
 			}
