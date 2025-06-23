b3ce1a85a1cf5797492173717d8387042e74f582
unknown <tuncel3@gmail.com>
2025-06-23 23:53:15 +0300

    Auto commit on 2025-06-23 23:53 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -786,30 +786,37 @@ void inline extern DEV_NOM_VOUT_changed_fc(void) {
 	set_dropper_l_lw_perc=EpD[SET_DROPP_L_LW_PERC][0].V1 / 100;
 }
 
+void inline extern IRECT_LIM_RT_changed_fc(void) {
+    Irect_max = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
+    Irect_min = EpD[IRECT_LIM_RT_][0].V1 * 0.01;
+    Ibat_max  = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
+    Ibat_min  = EpD[IRECT_LIM_RT_][0].V1 * 0.1;
+    blm_I_step_05perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.005;
+    blm_I_step_075perc = EpD[IRECT_LIM_RT_][0].V1 * 0.0075;
+    blm_I_step_10perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.010;
+}
+
 void inline extern set_variables_from_EEP_fc(uint8_t scope) { // n012
-    if (scope & SCOPE_CURRENT_LIMITS_FROM_EEP || scope == SCOPE_VAR_ALL_FROM_EEP) {
-        Irect_max = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
-        Irect_min = EpD[IRECT_LIM_RT_][0].V1 * 0.01;
-        Ibat_max  = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
-        Ibat_min  = EpD[IRECT_LIM_RT_][0].V1 * 0.1;
-    }
-    if (scope & SCOPE_BLM_LIMITS_FROM_EEP || scope == SCOPE_VAR_ALL_FROM_EEP || scope == SCOPE_DEV_NOM_VOUT_EEP) {
-        blm_I_step_05perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.005;
-        blm_I_step_075perc = EpD[IRECT_LIM_RT_][0].V1 * 0.0075;
-        blm_I_step_10perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.010;
-        blm_V_step_05perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.005;
-        blm_V_step_10perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.010;
-        blm_V_step_15perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.015;
-        blm_V_move_up_set  = EpD[DEV_NOM_VOUT][0].V1 * 0.02;
-        blm_V_move_dn_set  = EpD[DEV_NOM_VOUT][0].V1 * 0.02;
-    }
-    if (scope == SCOPE_VAR_ALL_FROM_EEP || scope == SCOPE_DEV_NOM_VOUT_EEP) {
-        VAC_Hg_Lim = VAC_Nom * (1 + 0.1); // Giriş voltajı monitör
-        VAC_Lo_Lim = VAC_Nom * (1 - 0.12); // Giriş voltajı monitör
-    }
-    if (scope == SCOPE_FAN_TEMP_EEP || scope == SCOPE_VAR_ALL_FROM_EEP) {
-		ovtmp_open_per=(uint32_t) (EpD[SET_OVT_OPEN_DELAY][0].V1*1000/50); // calculate alarm to open duration in 50ms
-    }
+	Vbat_flt = EpD[DEV_NOM_VOUT][0].V1 * 0.1;
+
+	Irect_max = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
+	Irect_min = EpD[IRECT_LIM_RT_][0].V1 * 0.01;
+	Ibat_max  = EpD[IRECT_LIM_RT_][0].V1 * 1.0;
+	Ibat_min  = EpD[IRECT_LIM_RT_][0].V1 * 0.1;
+
+	blm_I_step_05perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.005;
+	blm_I_step_075perc = EpD[IRECT_LIM_RT_][0].V1 * 0.0075;
+	blm_I_step_10perc  = EpD[IRECT_LIM_RT_][0].V1 * 0.010;
+	blm_V_step_05perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.005;
+	blm_V_step_10perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.010;
+	blm_V_step_15perc  = EpD[DEV_NOM_VOUT][0].V1 * 0.015;
+	blm_V_move_up_set  = EpD[DEV_NOM_VOUT][0].V1 * 0.02;
+	blm_V_move_dn_set  = EpD[DEV_NOM_VOUT][0].V1 * 0.02;
+
+	VAC_Hg_Lim = VAC_Nom * (1 + 0.1); // Giriş voltajı monitör
+	VAC_Lo_Lim = VAC_Nom * (1 - 0.12); // Giriş voltajı monitör
+
+	ovtmp_open_per=(uint32_t) (EpD[SET_OVT_OPEN_DELAY][0].V1*1000/50); // calculate alarm to open duration in 50ms
 }
 
 void inline extern set_V_targ_con_sy(float set_val) {
