890874f97e9b53abb945b767b7233377fbf987a2
unknown <tuncel3@gmail.com>
2025-06-23 20:47:10 +0300

    Auto commit on 2025-06-23 20:47 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -761,25 +761,20 @@ void inline extern aku_hatti_kopuk_fc_inl(void) {
 
 
 void inline extern DEV_NOM_VOUT_changed_fc(uint8_t scope) {
-    	EpD[VBAT_FLOAT][0].V1=EpD[DEV_NOM_VOUT][0].V1*1.1;	// şarj voltajlarını yeni belirlenen cihaz voltajına göre ayarla
-    	EpD[VBAT_FLOAT][1].V1=EpD[VBAT_FLOAT][0].V1;
-    	EpD[VBAT_BOOST][0].V1=EpD[DEV_NOM_VOUT][0].V1*1.15;
-    	EpD[VBAT_BOOST][1].V1=EpD[VBAT_BOOST][0].V1;
-        Vbat_flt = EpD[DEV_NOM_VOUT][0].V1 * 0.1;
+	EpD[VBAT_FLOAT][0].V1=EpD[DEV_NOM_VOUT][0].V1*1.1;	// şarj voltajlarını yeni belirlenen cihaz voltajına göre ayarla
+	EpD[VBAT_BOOST][0].V1=EpD[DEV_NOM_VOUT][0].V1*1.15;
+	Vbat_flt = EpD[DEV_NOM_VOUT][0].V1 * 0.1;
 	Vdc_float_min=EpD[DEV_NOM_VOUT][0].V1*0.9; // Normal şarj rejimi gerilim ayar aralığı
 	Vdc_float_max=EpD[DEV_NOM_VOUT][0].V1*1.15; // Normal şarj rejimi gerilim ayar aralığı
 	Vdc_boost_min=EpD[DEV_NOM_VOUT][0].V1*1.15; // Tam şarj rejimi gerilim ayar aralığı
 	Vdc_boost_max=EpD[DEV_NOM_VOUT][0].V1*1.3; // Tam şarj rejimi gerilim ayar aralığı
 	actions_after_charge_voltage_change();
-		blm_op_phase = B_SKIP_DELAY_RESTART;					// cancel op. bring_vtarg_back_skip_delay
-		blm_enable_collect_samples = 0;
-		blm_corr_buf_index = 0;
-
+	blm_op_phase = B_SKIP_DELAY_RESTART;					// cancel op. bring_vtarg_back_skip_delay
+	blm_enable_collect_samples = 0;
+	blm_corr_buf_index = 0;
 }
 
 void inline extern set_variables_from_EEP_fc(uint8_t scope) { // n012
-    if (scope & SCOPE_VOLTAGE_LIMITS_FROM_EEP || scope == SCOPE_VAR_ALL_FROM_EEP || scope == SCOPE_DEV_NOM_VOUT_EEP) {
-    }
     if (scope & SCOPE_DROPPER_LIMITS_FROM_EEP || scope == SCOPE_VAR_ALL_FROM_EEP || scope == SCOPE_DEV_NOM_VOUT_EEP) {
     	// D.A. gerilim regülasyonu (dropping diyot)
     	// limitler şartnamede anma geriliminin yüzdesi olarak belirtilmiş.
