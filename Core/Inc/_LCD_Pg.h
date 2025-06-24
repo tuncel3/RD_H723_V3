e64e32fa3206ee17475e0f0810cf44b7fa34ee40
unknown <tuncel3@gmail.com>
2025-06-24 23:57:31 +0300

    Auto commit on 2025-06-24 23:57 - auto

--- Core/Inc/_LCD_Pg.h
+++ Core/Inc/_LCD_Pg.h
@@ -302,15 +302,15 @@ if (tabl_dolas_delay_cnt==0) {
 		if (state_list[tabl_dolas].action & (1 << LCD_roll_enum) && state_list[tabl_dolas].action & (1 << ACTIVE_enum)) {
 			if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC) && EpD[SET_CHARGE_MODE][0].V1 == AUTO) {
 				sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Oto)");
-			} else if ((charge_mode_timed_boost) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
+			} else if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
 				sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Zmn)");
-			} else if ((charge_mode_timed_float) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
-				sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Zmn)");
+			} else if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC)) {
+				sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Man)");
 			} else {
 				sprintf(RollBuf, "%s", state_list[tabl_dolas].name);
 			}
 			son_kal=tabl_dolas+1;
-			break; // ilk gösterilecek eleman bulundu gösterildi.
+			break;
 		}
 	}
 	if (tabl_dolas == NUM_STATE_NAMES) {	// tablo sonuna gelirsek başa dön tara bulursan göster.
@@ -321,9 +321,7 @@ if (tabl_dolas_delay_cnt==0) {
 			if (state_list[tabl_dolas].action & (1 << LCD_roll_enum) && state_list[tabl_dolas].action & (1 << ACTIVE_enum)) {
 				if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC) && EpD[SET_CHARGE_MODE][0].V1 == AUTO) {
 					sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Oto)");
-				} else if ((charge_mode_timed_boost) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
-					sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Zmn)");
-				} else if ((charge_mode_timed_float) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
+				} else if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC) && EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
 					sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Zmn)");
 				} else if ((state_list[tabl_dolas].code==FLOAT_CHARGE_FC || state_list[tabl_dolas].code==BOOST_CHARGE_FC)) {
 					sprintf(RollBuf, "%s%s", state_list[tabl_dolas].name, "(Man)");
