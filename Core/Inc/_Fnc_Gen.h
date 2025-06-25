1fd0288b0df08f75885c5ee0f5bb51c7d9cab279
unknown <tuncel3@gmail.com>
2025-06-25 10:04:53 +0300

    Auto commit on 2025-06-25 10:04 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -883,13 +883,13 @@ void inline extern actions_after_charge_mode_change(uint8_t num) {
 		charge_mode_timed_time_sec=0; // ekrandaki timed mode kalan saniye değerini kaldır
 		PRF_GEN("BOOST charge mode %d", num);
 	} else if (EpD[SET_CHARGE_MODE][0].V1 == TIMED_FLOAT_CHARGE_FC) {
-		Current_charge_voltage=EpD[VBAT_BOOST][0].V1;
-		I_batt_targ_con_sy=EpD[SET_IBAT_BOOST][0].V1;
+		Current_charge_voltage=EpD[VBAT_FLOAT][0].V1;
+		I_batt_targ_con_sy=EpD[SET_IBAT_FLOAT][0].V1;
 		set_V_targ_con_sy(Current_charge_voltage);
-		apply_state_changes_f(FLOAT_CHARGE_FC, 0);
-		apply_state_changes_f(BOOST_CHARGE_FC, 1);
-		apply_state_changes_f(TIMED_FLOAT_CHARGE_FC, 0);
-		apply_state_changes_f(TIMED_BOOST_CHARGE_FC, 1);
+		apply_state_changes_f(FLOAT_CHARGE_FC, 1);
+		apply_state_changes_f(BOOST_CHARGE_FC, 0);
+		apply_state_changes_f(TIMED_FLOAT_CHARGE_FC, 1);
+		apply_state_changes_f(TIMED_BOOST_CHARGE_FC, 0);
 		apply_state_changes_f(MANUAL_CHARGE, 0);
 		apply_state_changes_f(AUTO_CHARGE, 0);
 		LED_7_Data &= ~FLOAT_CHARGE_LED;
