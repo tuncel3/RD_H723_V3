4170edfa67826afb39e9e292213cb14f5c0ad9d0
unknown <tuncel3@gmail.com>
2025-06-25 17:43:48 +0300

    Commit on 2025-06-25 17:43 - hh

--- Core/Inc/_Main.h
+++ Core/Inc/_Main.h
@@ -88,16 +88,16 @@ if (flt_array_index_found == 0) { // couldn't find last fault record location. C
 PRF_GEN("USE EEPROM TABLE DATA"); // eeprom okunduktan sonra kayıtlı dataya göre değişkenleri belirle
 PRF_GEN("Startup eeprom charge mode %f", EpD[SET_CHARGE_MODE][0]);
 if (EpD[SET_CHARGE_MODE][0].V1 == FLOAT) {
-	set_state_active(BOOST_CHARGE_FC);
+	set_state_active(FLOAT_CHARGE_FC);
 	temp_targ_DC_voltage=EpD[VBAT_FLOAT][0].V1;
 	targ_DC_current=EpD[SET_IBAT_FLOAT][0].V1;
 } else if (EpD[SET_CHARGE_MODE][0].V1 == BOOST) {
-	set_state_active(FLOAT_CHARGE_FC);
-} else if (EpD[SET_CHARGE_MODE][0].V1 == MANUAL) {
+	set_state_active(BOOST_CHARGE_FC);
+} else if (EpD[SET_CH_CONT_MODE][0].V1 == MANUAL) {
 	set_state_active(MANUAL_CHARGE_FC);
-} else if (EpD[SET_CHARGE_MODE][0].V1 == AUTO) {
+} else if (EpD[SET_CH_CONT_MODE][0].V1 == AUTO) {
 	set_state_active(AUTO_CHARGE_ST);
-} else if (EpD[SET_CHARGE_MODE][0].V1 == TIMED) {
+} else if (EpD[SET_CH_CONT_MODE][0].V1 == TIMED) {
 	set_state_active(TIMED_CHARGE_FC);
 }
 //LED_7_Data |= FLOAT_CHARGE_LED;
