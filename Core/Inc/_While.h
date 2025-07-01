f5967ff05f88ed5da3d98019302a38c2ccb3f876
unknown <tuncel3@gmail.com>
2025-07-01 11:52:02 +0300

    Auto commit on 2025-07-01 11:52 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -1049,15 +1049,15 @@ LED_16_Data &= ~(!state_get(VAC_ON_FC) << (VAC_ON_FC-0));
 //rel_out_16Bit_Data REL_OUT_TB[0].rel_out_tb_nm
 
 for (int i = 0; i < NM_STATE_CODES; i++) {
-	A=state_list[i].rel_ord == 3	// 4th column is 3
-	B=state_list[i].action & (1 << 3) // 3rd bit of the 2nd column is 1
-	C=rel_out_16Bit_Data |= (1 << (16-3)) // Set the 3rd bit from the start of rel_out_16Bit_Data
+//	A=state_list[i].rel_ord == 3	// 4th column is 3
+//	B=state_list[i].action & (1 << 3) // 3rd bit of the 2nd column is 1
+//	C=rel_out_16Bit_Data |= (1 << (16-3)) // Set the 3rd bit from the start of rel_out_16Bit_Data
 
     if (state_list[i].rel_ord == 3) { // Check if the 4th column is 3
-        if (state_list[i].action & (1 << 3)) { // Check if the 3rd bit of the 2nd column is 1
+        if (state_get(i)) { // Check if this column state is active
             rel_out_16Bit_Data |= (1 << (16-3)); // Set the 3rd bit from the start of rel_out_16Bit_Data
         }
-        else if (!(state_list[i].action & (1 << 3))) { // Check if the 3rd bit of the 2nd column is 1
+        else if (!state_get(i)) { // Check if the 3rd bit of the 2nd column is 1
             rel_out_16Bit_Data &= ~(1 << (16-3)); // Set the 3rd bit from the start of rel_out_16Bit_Data
         }
     }
