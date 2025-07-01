46c0120aec9bb1896ba0ec36fe44bf22de0a7be9
unknown <tuncel3@gmail.com>
2025-07-01 10:13:01 +0300

    Auto commit on 2025-07-01 10:13 - auto

--- Core/Inc/_While.h
+++ Core/Inc/_While.h
@@ -1049,6 +1049,13 @@ LED_16_Data &= ~(!state_get(VAC_ON_FC) << (VAC_ON_FC-0));
 //rel_out_16Bit_Data REL_OUT_TB[0].rel_out_tb_nm
 
 
+for (int i = 0; i < sizeof(state_list) / sizeof(state_list[0]); i++) {
+    if (state_list[i].rel_ord == 3) { // Check if the 4th column is 3
+        if (state_list[i].action & (1 << 3)) { // Check if the 3rd bit of the 2nd column is 1
+            rel_out_16Bit_Data |= (1 << 2); // Set the 3rd bit from the end of rel_out_16Bit_Data
+        }
+    }
+}
 
 
 } // if (ms_tick_cnt-while_delay50_h >= 50) {
