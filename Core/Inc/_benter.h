a83d45422714075d023589056cd15ad5a4144c11
unknown <tuncel3@gmail.com>
2025-07-02 10:21:54 +0300

    Auto commit on 2025-07-02 10:21 - auto

--- Core/Inc/_benter.h
+++ Core/Inc/_benter.h
@@ -296,15 +296,15 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==VRECT_DC_LOW_LIM_add) {
     	rel_disp_mode ^=1;
     	rel_edit_mode ^=1;
 
-//        if (rel_disp_mode == 1) { // ekranda order değiştirildi. tabloda değiş
-//        	REL_OUT_TB[rel_ord_tb_sel].rel_out_code= REL_DAT_TB[rel_dat_tb_sel].rel_dat_nm;
-//        	REL_OUT_TB[rel_ord_tb_sel].rel_out_tb_desc= REL_DAT_TB[rel_dat_tb_sel].rel_dat_desc; // REL_DAT_TB den seçili olanı al ve REL_OUT_TB ye kopyala
-//        	REL_OUT_TB[rel_ord_tb_sel].rel_out_tb_val= REL_DAT_TB[rel_dat_tb_sel].rel_dat_val;
-//        	generate_REL_OUT_order_vect_from_ord_table_fc(); // relout order vector üret
-//        	generate_REL_24Bit_Data_fc(); // 24 bit data üret, shift register için
-//        	save_REL_OUT_order_to_EEP(); //sıkıştırılmış orderı 20 bitlik parçalara ayır ve eeproma kaydet
-//        } else if (rel_edit_mode == 1) {
-//        }
+        if (rel_disp_mode == 1) { // ekranda order değiştirildi. tabloda değiş
+        	REL_OUT_TB[rel_ord_tb_sel].rel_out_code= REL_DAT_TB[rel_dat_tb_sel].rel_dat_nm;
+        	REL_OUT_TB[rel_ord_tb_sel].rel_out_tb_desc= REL_DAT_TB[rel_dat_tb_sel].rel_dat_desc; // REL_DAT_TB den seçili olanı al ve REL_OUT_TB ye kopyala
+        	REL_OUT_TB[rel_ord_tb_sel].rel_out_tb_val= REL_DAT_TB[rel_dat_tb_sel].rel_dat_val;
+        	generate_REL_OUT_order_vect_from_ord_table_fc(); // relout order vector üret
+        	generate_REL_24Bit_Data_fc(); // 24 bit data üret, shift register için
+        	save_REL_OUT_order_to_EEP(); //sıkıştırılmış orderı 20 bitlik parçalara ayır ve eeproma kaydet
+        } else if (rel_edit_mode == 1) {
+        }
     }
 
     else if (currentPage == MANAGEMENT_pg) {
