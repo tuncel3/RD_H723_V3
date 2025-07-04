78c197210dc95abfe4bf848deeb604f571cf35aa
unknown <tuncel3@gmail.com>
2025-07-04 21:20:46 +0300

    Auto commit on 2025-07-04 21:20 - auto

--- Core/Inc/_benter.h
+++ Core/Inc/_benter.h
@@ -297,7 +297,7 @@ else if (DEVICE_SETT_Items[selected_DEVICE_SETT].V1==VRECT_DC_LOW_LIM_add) {
     	rel_edit_mode ^=1;
 
         if (rel_disp_mode == 1) { // ekranda order değiştirildi. tabloda değiş
-//        	SIRALI_TABLO_RELOUT[sirali_tablo_sel].rel_out_code= TAM_TABLO_RELOUT[tam_tablo_sel].rel_dat_nm;
+        	SIRALI_TABLO_RELOUT[sirali_tablo_sel].rel_out_code= TAM_TABLO_RELOUT[tam_tablo_sel].rel_dat_nm;
 //        	SIRALI_TABLO_RELOUT[sirali_tablo_sel].rel_out_tb_desc= TAM_TABLO_RELOUT[tam_tablo_sel].rel_dat_desc; // REL_DAT_TB den seçili olanı al ve SIRALI_TABLO_RELOUT ye kopyala
 //        	SIRALI_TABLO_RELOUT[sirali_tablo_sel].rel_out_tb_val= TAM_TABLO_RELOUT[tam_tablo_sel].rel_dat_val;
         	generate_REL_OUT_order_vect_from_ord_table_fc(); // relout order vector üret
