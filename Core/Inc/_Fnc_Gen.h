6257c7b225eee51246a5e33030a7d176c8790330
unknown <tuncel3@gmail.com>
2025-07-05 06:30:30 +0300

    Auto commit on 2025-07-05 06:30 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -1155,24 +1155,27 @@ void save_REL_OUT_order_to_EEP(void) {
     REL_OUT_order_part3 = 0; // bu fonksiyon ile de 4 tane 30 bitlik sayıya dönüştürülerek eeproma kaydediliyor.
     REL_OUT_order_part4 = 0;
 	for (int i = 0; i < NM_STATE_CODES; i++) {
-		int bit_pos = state_list[i].rel_ord; // Get the rel_ord value
+		int rel_ord_ = state_list[i].rel_ord; // Get the rel_ord value
 		int state_code = state_list[i].code; // Get the state_code value
-		PRF_GEN("rel_ord code %u %u", bit_pos, state_code); delayA_1us(10);
+		PRF_GEN("rel_ord code %u %u", rel_ord_, state_code); delayA_1us(10);
 
-		if (bit_pos >= 0 && bit_pos <= 15) { // Ensure bit_pos is within the valid range
-			uint32_t val = state_code & 0x1F;  // 5-bit
-			PRF_GEN("  bit_pos 0-15 val %u %u", bit_pos, val); delayA_1us(10);
-			if (bit_pos < 4) { // her döngüde val değerlerini yan yana koyuyor
-				REL_OUT_order_part1 |= (val << (bit_pos * 5)); // 4 tane 5 bit sayıyı yan yana koyarak bir tane 20 bitlik eeprom kayıt dosyası oluşturuluyor
-			} else if (bit_pos < 8) {
-				REL_OUT_order_part2 |= (val << ((bit_pos - 4) * 5)); // i burda 4 5 6 7 oluyor. parantez içi 0 1 2 3 oluyor.
-			} else if (bit_pos < 12) {
-				REL_OUT_order_part3 |= (val << ((bit_pos - 8) * 5));
+		if (rel_ord_ >= 0 && rel_ord_ <= 15) { // Ensure rel_ord_ is within the valid range
+			uint32_t state_code_ = state_code & 0x1F;  // 5-bit
+			PRF_GEN("  rel_ord_ 0-15 state_code_ %u %u", rel_ord_, state_code_); delayA_1us(10);
+			if (rel_ord_ < 4) { // her döngüde state_code_ değerlerini yan yana koyuyor
+				REL_OUT_order_part1 |= (state_code_ << (rel_ord_ - 0 * 5)); // 4 tane 5 bit sayıyı yan yana koyarak bir tane 20 bitlik eeprom kayıt dosyası oluşturuluyor
+				PRF_GEN("     rel_ord_ state_code_ (state_code_ << (rel_ord_ * 5)) %u %u %u", rel_ord_, state_code_, (rel_ord_ - 0) * 5); delayA_1us(10);
+			} else if (rel_ord_ < 8) {
+				REL_OUT_order_part2 |= (state_code_ << ((rel_ord_ - 4) * 5)); // i burda 4 5 6 7 oluyor. parantez içi 0 1 2 3 oluyor.
+				PRF_GEN("     rel_ord_ state_code_ (state_code_ << (rel_ord_ * 5)) %u %u %u", rel_ord_, state_code_, (rel_ord_ - 4) * 5); delayA_1us(10);
+			} else if (rel_ord_ < 12) {
+				REL_OUT_order_part3 |= (state_code_ << ((rel_ord_ - 8) * 5));
+				PRF_GEN("     rel_ord_ state_code_ (state_code_ << (rel_ord_ * 5)) %u %u %u", rel_ord_, state_code_, (rel_ord_ - 8) * 5); delayA_1us(10);
 			} else {
-				REL_OUT_order_part4 |= (val << ((bit_pos - 12) * 5));
+				REL_OUT_order_part4 |= (state_code_ << ((rel_ord_ - 12) * 5));
+				PRF_GEN("     rel_ord_ state_code_ (state_code_ << (rel_ord_ * 5)) %u %u %u", rel_ord_, state_code_, (rel_ord_ - 12) * 5); delayA_1us(10);
 			}
-			PRF_GEN("     bit_pos val (val << (bit_pos * 5)) %u %u %u", bit_pos, val, (val << (bit_pos * 5)));
-			delayA_1us(10);
+
 		}
 	}
 
