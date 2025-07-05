c7d97b03424a9354429f84f360bec4ec68726603
unknown <tuncel3@gmail.com>
2025-07-05 07:54:34 +0300

    Auto commit on 2025-07-05 07:54 - auto

--- Core/Inc/_Fnc_Gen.h
+++ Core/Inc/_Fnc_Gen.h
@@ -1157,10 +1157,10 @@ void save_REL_OUT_order_to_EEP(void) {
     REL_OUT_order_part2 = 0;
     REL_OUT_order_part3 = 0;
     REL_OUT_order_part4 = 0;
-	for (int i = 0; i < NM_STATE_CODES; i++) { // tüm state list taranıyor
+	for (int i = 0; i < NM_STATE_CODES; i++) { // tüm state_list taranıyor
 		int rel_ord_ = state_list[i].rel_ord; // rel order al
 		uint8_t state_code_ = state_list[i].code & 0x1F; // state_code_ un 5-bit'lik kısmını al
-		if (rel_ord_ >= 0 && rel_ord_ <= 15) { // Ensure rel_ord_ is within the valid range
+		if (rel_ord_ >= 0 && rel_ord_ <= 15) { // state_list rel_ord kısmında 0 15 arası değerler olan satırlara bakılıyor
 			if (rel_ord_ < 4) { // her döngüde state_code_ değerlerini yan yana koyuyor
 				REL_OUT_order_part1 |= (state_code_ << ((rel_ord_ - 0) * 5)); // rel_ord_ 4 5 6 7, parantez içi 0 1 2 3 oluyor.
 				PRF_GEN("%u %u %u %d %lu", rel_ord_, state_code_, ((rel_ord_ - 0) * 5), (state_code_ << ((rel_ord_ - 0) * 5)), REL_OUT_order_part1); delayA_1us(10);
